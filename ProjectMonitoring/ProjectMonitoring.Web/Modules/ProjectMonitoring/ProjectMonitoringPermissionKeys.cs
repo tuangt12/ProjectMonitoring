ce4a4fb2380b8908469b8e41bc1b6c824d62ef23
Phan Anh Tuấn <promeranhtuan@gmail.com>
2019-03-19 15:23:48 +0700

    Thêm PemissionKey cho database namespace ProjectMonitoring.ProjectMonitoring Trong đó: ProjectMonitoring1 là namespace ProjectMonitoring2 là tên Module

---
+++ ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/ProjectMonitoringPermissionKeys.cs
@@ -0,0 +1,17 @@
+﻿
+using Serenity.Extensibility;
+using System.ComponentModel;
+
+namespace ProjectMonitoring.ProjectMonitoring
+{
+    [NestedPermissionKeys]
+    [DisplayName("PermissionKeys PM")]
+    public class PermissionKeys
+    {
+        [Description("User, Role Management and Permissions")]
+        public const string Security = "PM:Security";
+
+        [Description("Languages and Translations")]
+        public const string Translation = "PM:Translation";
+    }
+}
