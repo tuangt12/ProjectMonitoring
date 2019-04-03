5a4fb79cb06badedf8a16358be598f6f373e4198
Phan Anh Tuấn <promeranhtuan@gmail.com>
2019-04-04 00:26:57 +0700

    Thiết đặt quyền cho các bảng Classes, Posts, SCMs, SCMTypes, Subjects theo class

--- ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/ProjectMonitoringPermissionKeys.cs
+++ ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/ProjectMonitoringPermissionKeys.cs
@@ -13,15 +13,66 @@ namespace ProjectMonitoring.ProjectMonitoring
 
         [Description("Teacher Permissions")]
         public const string Teacher = "PM:Teacher";
-        
-        // Thiết đặt quyền cho bảng SCM
+
+        // Thiết đặt quyền cho bảng Classes
+        public class Class
+        {
+            [ImplicitPermission(General), ImplicitPermission(View)]
+            public const string Delete = "PM:Class:Delete";
+
+            [Description("Create/Update"), ImplicitPermission(General), ImplicitPermission(View)]
+            public const string Modify = "PM:Class:Modify";
+
+            public const string View = "PM:Class:View";
+        }
+
+        // Thiết đặt quyền cho bảng Posts
+        public class Post
+        {
+            [ImplicitPermission(General), ImplicitPermission(View)]
+            public const string Delete = "PM:Post:Delete";
+
+            [Description("Create/Update"), ImplicitPermission(General), ImplicitPermission(View)]
+            public const string Modify = "PM:Post:Modify";
+
+            public const string View = "PM:Post:View";
+        }
+
+        // Thiết đặt quyền cho bảng SCMs
         public class SCM
         {
             [ImplicitPermission(General), ImplicitPermission(View)]
             public const string Delete = "PM:SCM:Delete";
+
             [Description("Create/Update"), ImplicitPermission(General), ImplicitPermission(View)]
             public const string Modify = "PM:SCM:Modify";
+
             public const string View = "PM:SCM:View";
         }
+
+        // Thiết đặt quyền cho bảng SCMTypes
+        public class SCMType
+        {
+            [ImplicitPermission(General), ImplicitPermission(View)]
+            public const string Delete = "PM:SCMType:Delete";
+
+            [Description("Create/Update"), ImplicitPermission(General), ImplicitPermission(View)]
+            public const string Modify = "PM:SCMType:Modify";
+
+            public const string View = "PM:SCMType:View";
+        }
+
+        // Thiết đặt quyền cho bảng Subjects
+        public class Subject
+        {
+            [ImplicitPermission(General), ImplicitPermission(View)]
+            public const string Delete = "PM:Subject:Delete";
+
+            [Description("Create/Update"), ImplicitPermission(General), ImplicitPermission(View)]
+            public const string Modify = "PM:Subject:Modify";
+
+            public const string View = "PM:Subject:View";
+        }
+
     }
 }
