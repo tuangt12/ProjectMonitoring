4c97aeaf56ec124c677c6554f06e557895065c74
Phan Anh Tuấn <promeranhtuan@gmail.com>
2019-04-23 02:06:03 +0700

    Hiển thị trực tiếp ClassCode và SubjectCode lên giao diện ngay khi load

--- ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesEditor.ts
+++ ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesEditor.ts
@@ -15,5 +15,22 @@ namespace ProjectMonitoring.ProjectMonitoring {
         protected getAddButtonCaption() {
             return "Add to a Class";
         }
+
+        protected validateEntity(row: UserClassesRow, id: number) {
+            if (!super.validateEntity(row, id))
+                return false;
+
+            // Gán trường ClassCode của UserClass hiện tại bằng cách
+            // Lấy ra trường ClassCode trong ClassesRow theo Id 
+            // (Id này chính là ClassId lấy từ row hiện tại, Row hiện tại là Row của UserClasses)
+            row.ClassClassCode = ClassesRow.getLookup()
+                .itemById[row.ClassId].ClassCode;
+
+            // Gán trường SubjectCode của UserClass hiện tại
+            row.ClassSubjectCode = ClassesRow.getLookup()
+                .itemById[row.ClassId].SubjectCode;
+
+            return true;
+        } 
     }
 }
