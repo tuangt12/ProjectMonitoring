51a59ac573960e1382dfdd48b12a4a70495059fd
Phan Anh Tuấn <promeranhtuan@gmail.com>
2019-04-22 16:48:54 +0700

    Thêm Dialog cho phép thêm User cào 1 Class

--- ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesEditor.ts
+++ ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesEditor.ts
@@ -5,9 +5,15 @@ namespace ProjectMonitoring.ProjectMonitoring {
     export class UserClassesEditor
         extends Common.GridEditorBase<UserClassesRow> {
         protected getColumnsKey() { return "ProjectMonitoring.UserClasses"; }
+        protected getDialogType() { return UserClassesEditDialog; }
         protected getLocalTextPrefix() { return UserClassesRow.localTextPrefix; }
+
         constructor(container: JQuery) {
             super(container);
+        }
+
+        protected getAddButtonCaption() {
+            return "Add to a Class";
         }
     }
 }
