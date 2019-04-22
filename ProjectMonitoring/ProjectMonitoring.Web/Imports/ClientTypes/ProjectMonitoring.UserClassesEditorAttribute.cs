bec9ffe7e43c4af66f24d9c20f7ec46b89b7b57f
Phan Anh Tuấn <promeranhtuan@gmail.com>
2019-04-22 15:39:43 +0700

    Khai báo thuộc tính Attribute cho UserClassesEditor để có thể sử dụng ở các vị trí khác

---
+++ ProjectMonitoring/ProjectMonitoring.Web/Imports/ClientTypes/ProjectMonitoring.UserClassesEditorAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Serenity;
+using Serenity.ComponentModel;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace ProjectMonitoring.ProjectMonitoring
+{
+    public partial class UserClassesEditorAttribute : CustomEditorAttribute
+    {
+        public const string Key = "ProjectMonitoring.ProjectMonitoring.UserClassesEditor";
+
+        public UserClassesEditorAttribute()
+            : base(Key)
+        {
+        }
+    }
+}
+
