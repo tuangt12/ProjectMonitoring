4c76f1964a389205d24bf96eca788915d301b68d
Phan Anh Tuấn <promeranhtuan@gmail.com>
2019-04-22 15:39:12 +0700

    Thêm Editor, để từ 1 user có thể đăng ký Class cho chính mình. Đây là bảng hiển thị thông tin sinh viên và lớp tương ứng.

---
+++ ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesEditor.ts
@@ -0,0 +1,13 @@
+﻿
+/// <reference path="../../Common/Helpers/GridEditorBase.ts" />
+namespace ProjectMonitoring.ProjectMonitoring {
+    @Serenity.Decorators.registerEditor()
+    export class UserClassesEditor
+        extends Common.GridEditorBase<UserClassesRow> {
+        protected getColumnsKey() { return "ProjectMonitoring.UserClasses"; }
+        protected getLocalTextPrefix() { return UserClassesRow.localTextPrefix; }
+        constructor(container: JQuery) {
+            super(container);
+        }
+    }
+}
