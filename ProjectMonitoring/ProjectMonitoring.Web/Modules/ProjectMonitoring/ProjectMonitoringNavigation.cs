ca86e081fd2431593483d883763d2de03f9a1cd7
Phan Anh Tuấn <tuangt12@gmail.com>
2019-06-04 01:24:23 +0700

    - Bổ sung icon trong Navigation - Dịch 2 mục Attach File sang tiếng Việt

--- ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/ProjectMonitoringNavigation.cs
+++ ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/ProjectMonitoringNavigation.cs
@@ -9,10 +9,11 @@ using Administration = ProjectMonitoring.Administration.Pages;
 [assembly: NavigationMenu(2000, "ProjectMonitoring", icon: "fa-code")]
 [assembly: NavigationLink(2100, "ProjectMonitoring/Account", typeof(Administration.UserController), icon: "fa-user")]
 [assembly: NavigationLink(2200, "ProjectMonitoring/Posts", typeof(MyPages.PostsController), icon: "fa-align-left")]
-[assembly: NavigationLink(2300, "ProjectMonitoring/Classes", typeof(MyPages.ClassesController), icon: "fa-graduation-cap")]
-[assembly: NavigationLink(2400, "ProjectMonitoring/Subjects", typeof(MyPages.SubjectsController), icon: "fa-book")]
-[assembly: NavigationLink(2500, "ProjectMonitoring/SCMs", typeof(MyPages.SCMsController), icon: "fa-link")]
-[assembly: NavigationLink(2600, "ProjectMonitoring/SCMTypes", typeof(MyPages.SCMTypesController), icon: "fa-github-square")]
-[assembly: NavigationLink(2700, "ProjectMonitoring/User Classes", typeof(MyPages.UserClassesController), icon: null)]
+[assembly: NavigationLink(2300, "ProjectMonitoring/User Classes", typeof(MyPages.UserClassesController), icon: "fa-file-text")]
+[assembly: NavigationLink(2400, "ProjectMonitoring/Classes", typeof(MyPages.ClassesController), icon: "fa-graduation-cap")]
+[assembly: NavigationLink(2500, "ProjectMonitoring/Subjects", typeof(MyPages.SubjectsController), icon: "fa-book")]
+[assembly: NavigationLink(2600, "ProjectMonitoring/SCMs", typeof(MyPages.SCMsController), icon: "fa-link")]
+[assembly: NavigationLink(2700, "ProjectMonitoring/SCMTypes", typeof(MyPages.SCMTypesController), icon: "fa-github-square")]
+
 
 
