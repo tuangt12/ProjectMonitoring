4929378aa90d929551151dfd7bc16814cc4e2a6e
Phan Anh Tuấn <tuangt12@gmail.com>
2019-06-03 18:54:37 +0700

    - Tự động chèn các trường Display Name và Subjects Name khi select tại dropdown phía trên. - Hiển thị/ Ẩn các button và Filter theo phân quyền tương ứng.

--- ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesDialog.ts
+++ ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesDialog.ts
@@ -2,6 +2,7 @@
 namespace ProjectMonitoring.ProjectMonitoring {
 
     @Serenity.Decorators.registerClass()
+    @Serenity.Decorators.maximizable()
     export class UserClassesDialog extends Serenity.EntityDialog<UserClassesRow, any> {
         protected getFormKey() { return UserClassesForm.formKey; }
         protected getIdProperty() { return UserClassesRow.idProperty; }
@@ -10,5 +11,45 @@ namespace ProjectMonitoring.ProjectMonitoring {
 
         protected form = new UserClassesForm(this.idPrefix);
 
+        constructor() {
+            super();
+            this.form = new UserClassesForm(this.idPrefix);
+
+            // Tự động điền tên môn học khi chọn lớp
+            this.form.ClassId.changeSelect2(e => {
+                var classID = Q.toId(this.form.ClassId.value);
+                if (classID != null) {
+
+                    this.form.ClassSubjectCode.value = ClassesRow.getLookup().itemById[classID].SubjectName;
+                }
+            });
+
+            // Tự động điền tên sinh viên khi chọn mã số sinh viên
+            this.form.UserId.changeSelect2(e => {
+                var userId = Q.toId(this.form.UserId.value);
+                if (userId != null) {
+
+                    this.form.UserDisplayName.value = Administration.UserRow.getLookup().itemById[userId].DisplayName;
+                }
+            });
+        }
+
+        // Thay đổi giao diện
+        // Khi đăng nhập với tư cách không phải admin thì sẽ không cho phép nhập/ chỉnh sửa điểm
+        protected updateInterface(): void {
+            super.updateInterface();
+            // Nếu kiểm tra mà không có quyền admin
+            if (!Authorization.hasPermission("Administration:Security")) {
+                // Tìm tới trường Name, Point và disable
+                // Vì các trường bên trên đã mặc định ở trạng thái không cho phép cập nhật rồi
+                // mà khi không có quyền Create New thì chắc chắn các trường khác sẽ ở dạng disable
+                Serenity.EditorUtils.setReadonly(this.form.Name.element, true);
+                Serenity.EditorUtils.setReadonly(this.form.Point.element, true);
+                // Ẩn tất cả các button Save/ Delete
+                this.toolbar.findButton('save-and-close-button').toggle(false);
+                this.toolbar.findButton('apply-changes-button').toggle(false);
+                this.toolbar.findButton('delete-button').toggle(false)
+            }
+        }
     }
 }
--- ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesGrid.ts
+++ ProjectMonitoring/ProjectMonitoring.Web/Modules/ProjectMonitoring/UserClasses/UserClassesGrid.ts
@@ -12,5 +12,30 @@ namespace ProjectMonitoring.ProjectMonitoring {
         constructor(container: JQuery) {
             super(container);
         }
+
+        // Không hiển thị nút thêm Topic mới tại đây khi đăng nhập với quyền sinh viên
+        // Sinh viên nếu muốn đăng ký một lớp mới (đăng ký đề tài) thì đã có trong phần quản lý tài khoản của mình
+        protected getButtons(): Serenity.ToolButton[] {
+            var buttons = super.getButtons();
+            // Nếu kiểm tra mà không có quyền admin
+            if (!Authorization.hasPermission("Administration:Security")) {
+                buttons.splice(Q.indexOf(buttons, x => x.cssClass == "add-button"), 1);
+            }
+            return buttons;
+        }
+
+        // Chỉ hiển thị các filter khi đăng nhập với quyền quản trị
+        // Khi đăng nhập với tư cách là sinh viên thì đã mặc định chỉ lọc các lớp mà sinh viên đó tham gia rồi
+        // nên không cần filter nữa
+        protected getQuickFilters() {
+
+            var filters = super.getQuickFilters();
+            // Kiểm tra quyền
+            if (!Authorization.hasPermission("Administration:Security")) {
+                // Trả về list filter rỗng (xóa tất cả các filter)
+                return [];
+            }
+            return filters;
+        }
     }
 }
